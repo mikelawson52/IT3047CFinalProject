6954d4d3817d68eb14bd3db62d390491d70c63b2
Michael_Lawson <lawsonm6@mail.uc.edu>
2019-04-19 16:16:05 -0400

    Cleaned up the tests

--- GroceryStoreSimulator/OnlineOrderingWebPage/Default.aspx.cs
+++ GroceryStoreSimulator/OnlineOrderingWebPage/Default.aspx.cs
@@ -9,22 +9,20 @@ public partial class _Default : System.Web.UI.Page {
     protected void Page_Load(object sender, EventArgs e) {
         using(GroceryStoreSimulatorContext context = new GroceryStoreSimulatorContext())
         {
-            //All DB Context's work.  Need to build forign key relationships still.
-        /******
-            var test1 = context.Empl.Take(100).ToList();
-            var test2 = context.EmplStatus.ToList();
-            var test3 = context.EmplTitle.ToList();
-            var test4 = context.Loyalty.ToList();
-            var test5 = context.LoyaltyStatus.ToList();
-            var test6 = context.Order.ToList();
-            var test7 = context.OrderDetail.ToList();
-            var test8 = context.OrderStatus.ToList();
-            var test9 = context.Product.ToList();
-            var test10 = context.Store.ToList();
-            var test11 = context.StoreStatus.ToList();
-            var test12 = context.Transaction.Take(100).ToList();
-            var test13 = context.TransactionDetail.Take(100).ToList();
-            ***/
+            //All DB Context Tests.
+            //var test1 = context.Empl.Take(100).ToList();
+            //var test2 = context.EmplStatus.ToList();
+            //var test3 = context.EmplTitle.ToList();
+            //var test4 = context.Loyalty.ToList();
+            //var test5 = context.LoyaltyStatus.ToList();
+            //var test6 = context.Order.ToList();
+            //var test7 = context.OrderDetail.ToList();
+            //var test8 = context.OrderStatus.ToList();
+            //var test9 = context.Product.ToList();
+            //var test10 = context.Store.ToList();
+            //var test11 = context.StoreStatus.ToList();
+            //var test12 = context.Transaction.Take(100).ToList();
+            //var test13 = context.TransactionDetail.Take(100).ToList();
             //var test14 = context.StoreHistory.Take(500).ToList();
             //var test15 = context.Order
             //    .Include("Store")
